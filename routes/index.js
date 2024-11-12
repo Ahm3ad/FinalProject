f00f7c4c96cd89c2907d33aa5783ceb7ac63d074
Ahmaad Riaz <ahmaad.riaz@ontariotechu.net>
2024-11-11 23:51:20 -0500

    creating a route for the incident form
    route for incident form

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,31 @@
-// Load installed third-party packages
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 // Render the home page
-router.get('/', function(req, res, next) {
-  res.render('index', { 
-    title: 'Home',
-     content: '<p>Welcome to the Car Incident Tracker! Track and manage incidents with ease.</p>'
-  });
-});
-
-// Redirect '/home' to the root URL to avoid duplication
-router.get('/home', function(req, res, next) {
-  res.redirect('/');
+router.get('/', (req, res) => {
+    res.render('index', { title: 'Home' });
 });
 
 // Render the about page
-router.get('/about', function(req, res, next) {
-  res.render('index', { 
-    title: 'About Us'
-  });
+router.get('/about', (req, res) => {
+    res.render('index', { title: 'About Us' });
 });
 
 // Render the projects page
-router.get('/projects', function(req, res, next) {
-  res.render('index', { 
-    title: 'Reported Incidents'
-  });
+router.get('/projects', (req, res) => {
+    res.render('index', { title: 'Reported Incidents' });
 });
 
 // Render the contact page
-router.get('/contact', function(req, res, next) {
-  res.render('index', { 
-    title: 'Contact Us'
-  });
+router.get('/contact', (req, res) => {
+    res.render('index', { title: 'Contact Us' });
 });
+
+router.get('/incidents', (req, res) => {
+    res.render('incident_form', { incidents: [] } )});
+
+
+
+
 
 module.exports = router;
